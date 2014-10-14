f8f81de3895211517bf3102df562a56b4cb068f8
Nikolay Ivanushkin <nikolay.ivanushkin@inn.ru>
2014-10-14 11:56:51 +0400

    Formatted code

--- src/ru/nim579/nixieclock/About.java
+++ src/ru/nim579/nixieclock/About.java
@@ -9,9 +9,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class About extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.about);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
--- src/ru/nim579/nixieclock/TimePickerPreference.java
+++ src/ru/nim579/nixieclock/TimePickerPreference.java
@@ -13,90 +13,90 @@ import android.widget.TimePicker;
 
 public class TimePickerPreference extends DialogPreference {
 
-	private static final String DEFAULT_TIME = "00:00";
+    private static final String DEFAULT_TIME = "00:00";
 
-	private TimePicker mTimePicker;
+    private TimePicker mTimePicker;
 
-	public TimePickerPreference(Context context, AttributeSet attrs, int defStyle){
-		super(context, attrs, defStyle);
-		setPositiveButtonText(R.string.general_ok);
-		setNegativeButtonText(R.string.general_cancel);
-	}
+    public TimePickerPreference(Context context, AttributeSet attrs, int defStyle){
+        super(context, attrs, defStyle);
+        setPositiveButtonText(R.string.general_ok);
+        setNegativeButtonText(R.string.general_cancel);
+    }
 
-	public TimePickerPreference(Context context, AttributeSet attrs){
-		this(context, attrs, 0);
-	}
+    public TimePickerPreference(Context context, AttributeSet attrs){
+        this(context, attrs, 0);
+    }
 
-	public TimePickerPreference(Context context){
-		this(context, null);
-	}
+    public TimePickerPreference(Context context){
+        this(context, null);
+    }
 
-	@Override
-	protected View onCreateDialogView(){
-		mTimePicker = new TimePicker(getContext());
-		mTimePicker.setEnabled(true);
-		return mTimePicker;
-	}
+    @Override
+    protected View onCreateDialogView(){
+        mTimePicker = new TimePicker(getContext());
+        mTimePicker.setEnabled(true);
+        return mTimePicker;
+    }
 
-	@Override
-	protected void onBindDialogView(View view) {
-		super.onBindDialogView(view);
-		TimePicker timePicker = mTimePicker;
-		int[] hourAndMinutes = getHourAndTime(getPersistedString(DEFAULT_TIME));
-		timePicker.setCurrentHour(hourAndMinutes[0]);
-		timePicker.setCurrentMinute(hourAndMinutes[1]);
-	}
+    @Override
+    protected void onBindDialogView(View view) {
+        super.onBindDialogView(view);
+        TimePicker timePicker = mTimePicker;
+        int[] hourAndMinutes = getHourAndTime(getPersistedString(DEFAULT_TIME));
+        timePicker.setCurrentHour(hourAndMinutes[0]);
+        timePicker.setCurrentMinute(hourAndMinutes[1]);
+    }
 
-	@Override
-	protected Object onGetDefaultValue(TypedArray a, int index) {
-		return a.getString(index);
-	}
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getString(index);
+    }
 
-	@Override
-	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-		persistString(restoreValue ? getPersistedString(getCurrentTime())
-				: (String) defaultValue);
-	}
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        persistString(restoreValue ? getPersistedString(getCurrentTime())
+                : (String) defaultValue);
+    }
 
-	protected void onDialogClosed(boolean positiveResult){
-		if (positiveResult){
-			if (mTimePicker != null){
-				persistString(getCurrentTime());
-			}
-		}
-	}
+    protected void onDialogClosed(boolean positiveResult){
+        if (positiveResult){
+            if (mTimePicker != null){
+                persistString(getCurrentTime());
+            }
+        }
+    }
 
-	/**
-	 * 
-	 * 
-	 * 
-	 * @return string in format HH:MM
-	 */
+    /**
+     * 
+     * 
+     * 
+     * @return string in format HH:MM
+     */
 
-	private String getCurrentTime(){
-		if (mTimePicker != null) {
-			return mTimePicker.getCurrentHour() + ":"+ mTimePicker.getCurrentMinute();
-		}
-		return DEFAULT_TIME;
-	}
+    private String getCurrentTime(){
+        if (mTimePicker != null) {
+            return mTimePicker.getCurrentHour() + ":"+ mTimePicker.getCurrentMinute();
+        }
+        return DEFAULT_TIME;
+    }
 
-	/**
-	 * 
-	 * 
-	 * 
-	 * @param timeString
-	 *            string in format HH:MM
-	 * 
-	 * @return array of size 2; first element - hours, second - minutes
-	 */
+    /**
+     * 
+     * 
+     * 
+     * @param timeString
+     *            string in format HH:MM
+     * 
+     * @return array of size 2; first element - hours, second - minutes
+     */
 
-	public static int[] getHourAndTime(String timeString){
-		if (!timeString.contains(":")) {
-			throw new RuntimeException("Bad time string");
-		}
-		String[] values = timeString.split(":");
-		return new int[] { Integer.parseInt(values[0]), Integer.parseInt(values[1]) };
+    public static int[] getHourAndTime(String timeString){
+        if (!timeString.contains(":")) {
+            throw new RuntimeException("Bad time string");
+        }
+        String[] values = timeString.split(":");
+        return new int[] { Integer.parseInt(values[0]), Integer.parseInt(values[1]) };
 
-	}
+    }
 
 }
